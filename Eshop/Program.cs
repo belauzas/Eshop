0684c4cbf8d1f78addf5ab1ab4592d582caf5d2a
Rimantas <belauzas@yahoo.com>
2020-12-18 01:25:59 +0200

    Add project files.

---
+++ Eshop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Eshop
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the shop!");
+
+            Shop shootsEshop = new Shop();
+
+            shootsEshop.AddItem("Pirstines");
+            shootsEshop.AddItem("Kepure");
+            shootsEshop.AddItem("Salikas");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Eshop/Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop
+{
+    public class Shop
+    {
+        private List<string> shopItems = new List<string>();
+
+        public int MyProperty { get; set; }
+
+        public void AddItem(string itemName)
+        {
+            shopItems.Add(itemName);
+            displayItems();
+            Console.WriteLine("-------------------");
+        }
+
+        public void displayItems()
+        {
+            foreach (var item in shopItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
